bffda1d2eee4c00bbcc38c3914a1ba1068579373
Pengwin3 <devin.hagerty95@gmail.com>
2017-02-07 20:02:47 -0500

    first submission

--- Schedule.cpp
+++ Schedule.cpp
@@ -8,32 +8,127 @@ using namespace std::rel_ops;
  *
  */
 Schedule::Schedule()
-    :head(nullptr),
-     tail(nullptr),
-     totalCredits(0)
+        : head(nullptr),
+        tail(nullptr),
+        totalCredits(0)
 {
 }
 
 Schedule::Schedule(const Schedule &src)
- :head(nullptr),
-  tail(nullptr),
-  totalCredits(0)
+        : head(nullptr),
+        tail(nullptr),
+        totalCredits(0)
 {
- Node* it = src.head;
- while (it!= nullptr)
-   {  this->add(it->data);
-     it = it->next; }
+        Node* it = src.head;
+        while (it!= nullptr)
+        {  this->add(it->data);
+           it = it->next; }
 }
 
 Schedule::~Schedule()
-{}
+{
+        // Deallocate the Linked List
+        Node* it = this->head;
 
- /**
- *
- */
+        while (it != nullptr) {
+                Node* prev = it;
+
+                it = it->next;
+                delete prev;
+        }
+        it = nullptr;
+
+        this->head = nullptr;
+        this->tail = nullptr;
+        // End Linked List Deallocation
+}
+
 bool Schedule::add(Course course)
 {
-  return true;
+        if(head==nullptr)
+        {
+                head = new Node(course);
+                tail = head;
+                totalCredits = course.getCredits();
+
+                return true;
+        }
+        else
+        {
+                bool kill = false;
+                Node *present = head;
+                if(totalCredits + course.getCredits() > CREDIT_LIMIT)
+                {
+                        return false;
+                }
+
+                while(present != nullptr && kill == false)
+                {
+                        if(course == present->data)
+                        {
+                                kill = true;
+                        }
+
+                        present = present->next;
+                }
+                if(kill == true)
+                {
+                        return false;
+                }
+                else
+                {
+                        tail->next = new Node(course);
+                        tail = tail->next;
+                        totalCredits += course.getCredits();
+
+                        return true;
+                }
+                return false;
+        }
+        // Course prev;
+        // int CRN = course.getCrn();
+        // std::string NUMBER = course.getNumber();
+        //
+        // if(head == nullptr)
+        // {
+        //         tail = head = new Node(course);
+        //         totalCredits = totalCredits + 3;
+        //         return true;
+        // }
+        // else
+        // {
+        //         Node *step = head;
+        //         bool kill = false;
+        //         while(step != nullptr && kill == false)
+        //         {
+        //                 if(course == step->data)
+        //                 {
+        //                         kill = true;
+        //                 }
+        //         }
+        //         if (kill == true)
+        //         {
+        //                 return false;
+        //         }
+        //         else
+        //         {
+        //                 int courseCredits = course.getCredits();
+        //                 if((totalCredits + courseCredits) > 12)
+        //                 {
+        //                         return false;
+        //                 }
+        //                 else if((totalCredits + courseCredits) <= 12)
+        //                 {
+        //                         tail->next = new Node(course);
+        //                         tail = tail->next;
+        //                         totalCredits += courseCredits;
+        //                         return true;
+        //                 }
+        //         }
+        //         step = step->next;
+        // }
+        //
+        // return true;
 }
 
 /**
@@ -41,15 +136,18 @@ bool Schedule::add(Course course)
  */
 void Schedule::display(std::ostream& outs) const
 {
-    Node* it = head;
+        Node* it = head;
 
-    outs << "  (" << totalCredits << " Total Credits)" << "\n";
+        outs << "  (" << totalCredits << " Total Credits)" << "\n";
 
-    while (it != nullptr) {
-        // oops I forgot to remove most of this function
+        while (it != nullptr) {
+                outs << "  - "
+                     << it->data.getCredits() << " Credits - "
+                     << it->data.getNumber() << " (CRN "
+                     << it->data.getCrn() << ")\n";
 
-        it = it->next;
-    }
+                it = it->next;
+        }
 }
 
 
@@ -57,12 +155,28 @@ void Schedule::display(std::ostream& outs) const
 /**
  * You may ignore this function, or use it, the choice is yours
  */
-void Schedule::deAllocateList(){}
+void Schedule::deAllocateList()
+{
+        // Deallocate the Linked List
+        Node* it = this->head;
+
+        while (it != nullptr) {
+                Node* prev = it;
+
+                it = it->next;
+                delete prev;
+        }
+        it = nullptr;
+
+        this->head = nullptr;
+        this->tail = nullptr;
+        // End Linked List Deallocation
+}
 
 /**
  * You may ignore this function, or use it, the choice is yours
  */
 void Schedule::copyList(const Schedule& rhs)
 {
-
+  // I choose to ignore it
 }
